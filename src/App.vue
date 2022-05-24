207450bfc2d63e8178edcbd21d8ea0e97c2c14cb
Aundre Bryan <48803238+MAE-BK201@users.noreply.github.com>
2022-05-24 18:15:44 -0500

    update: added Ar.js

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <a-scene embedded arjs>
+    <!-- <a-marker type="pattern" url="./assets/pattern-89.patt"> -->
+    <a-marker preset="hiro">
+      <!-- <a-entity -->
+      <!--   position="0 0 0" -->
+      <!--   scale="0.05 0.05 0.05" -->
+      <!--   gltf-model="https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/scene.gltf" -->
+      <!-- /> -->
+      <a-box />
+    </a-marker>
+    <a-entity camera />
+  </a-scene>
 </template>
 
 <style>
